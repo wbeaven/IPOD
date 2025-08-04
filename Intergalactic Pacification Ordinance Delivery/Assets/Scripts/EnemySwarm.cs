27d0197f15fc08ece1e8e8183744fd367992563a
Will-remote <wtbeaven@gmail.com>
2025-08-05 00:21:34 +1000

    Added swarm test scene & basic swarm script

---
+++ Intergalactic Pacification Ordinance Delivery/Assets/Scripts/EnemySwarm.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class EnemySwarm : MonoBehaviour
+{
+    [SerializeField] Transform[] drones;
+    [SerializeField] float speed = 5f;
+    private Transform player;
+    private Vector3 targetDirection;
+    private LayerMask playerLayer;
+    private bool detected;
+    private bool caught;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        playerLayer = LayerMask.NameToLayer("Player");
+    }
+
+    private void Update()
+    {
+        if (!caught)
+        {
+            DetectPlayer();
+            if (detected)
+            {
+                PlayerDetected();
+            }
+        }
+        else
+        {
+            PlayerCaught();
+        }
+    }
+
+    private void DetectPlayer()
+    {
+        targetDirection = transform.position - player.position;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, targetDirection, out hit, Mathf.Infinity, playerLayer))
+        {
+            detected = true;
+        }
+        else
+        {
+            detected = false;
+        }
+    }
+
+    private void PlayerDetected()
+    {
+        transform.LookAt(player.position);
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+
+        if (Vector3.Distance(transform.position, player.position) < 0.001f)
+        {
+            caught = true;
+        }
+    }
+
+    private void PlayerCaught()
+    {
+        transform.position = player.position;
+        // make drones orbit around
+        // start buzzing sound effects
+        // start applying damage for each drone alive
+    }
+}
